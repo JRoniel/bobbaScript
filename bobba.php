ae932ee227af54d4092c88e3b782bbe7e87acf14
JRoniel <60148238+JRoniel@users.noreply.github.com>
2020-01-21 19:43:29 -0300

    Update bobba.php

--- bobba.php
+++ bobba.php
@@ -1,13 +1,13 @@
 <?php
 
+//Função para bloquear e transcrever palavrões em frases. 
 function receiver(String $text) {
-$badword = ['porra','caralho','cuzão','bct','buceta','caraleo','vadia','desgraça','desgraca','noob','pinto','pau','xota','xoxota','maconha','fuder','puta','transar','tranzar','esfolar','arromba','arrombado','foda','foda-se','porr@','caralh@'];
-for($i = 0; $i < count($badword); $i++){
-$text = str_ireplace($badword[$i], 'bobba', $text);
-}
-return $text;
+  //Insira abaixo a lista de palavras a serem bloqueadas nas frases
+  $badword = ['porra','caralho','cuzão','bct','buceta','caraleo','vadia','desgraça','desgraca','noob','pinto','pau','xota','xoxota','maconha','fuder','puta','transar','tranzar','esfolar','arromba','arrombado','foda','foda-se','porr@','caralh@'];
+  for($i = 0; $i < count($badword); $i++){
+    $text = str_ireplace($badword[$i], 'bobba', $text);
+  }
+  return $text;
 }
 
-
-echo receiver('Seu filho da puta do caralho');
 ?>
