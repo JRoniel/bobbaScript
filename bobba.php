4a56b77883473c9c999aa85d7f19c7bfc3377e8e
JRoniel <60148238+JRoniel@users.noreply.github.com>
2020-01-22 11:15:20 -0300

    Update bobba.php
    √ Correções rápidas

--- bobba.php
+++ bobba.php
@@ -8,23 +8,29 @@
   •Meu twitter é @zBanXD_
 */
 
-//Função para bloquear e transcrever palavrões em frases. 
-function receiver(String $text) {
-  //Insira abaixo a lista de palavras a serem bloqueadas nas frases
+
+//Função para receber frase a ser corrigida. 
+function receiver(String $text)
+{
+  
+  //Lista de badwords. 
   $badword = ['porra','caralho','cuzão','bct','buceta','caraleo','vadia','desgraça',
   'desgraca','pinto','pau','xota','xoxota','maconha','fuder','puta','transar',
   'tranzar','esfolar','arromba','arrombado','foda','foda-se','porr@','caralh@','krl',
   'fdp','gay','viado','viadinho','cu','penis','tmnc','rapariga','consolo','bucet@','bucyta','desgraça'];
-  for($i = 0; $i < count($badword); $i++){
+  
+  for($i = 0; $i < count($badword); $i++)
+  {
     $text = str_ireplace($badword[$i], 'bobba', $text);
   }
+  
   //Frase retorna pronta para uso. 
   return $text;
 }
 
 
 /**
-  Instruções:
+  Estruções:
     Frase enviada: Vai para a casa do caralh*! 
     
     você vai repassar essa frase em forma de String para a função. 
